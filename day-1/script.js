334ae721c4a3473f2a5bf87da5411613b1130860
Md Sabuj Mia <mdsobujredoy@gmail.com>
2025-11-01 14:52:32 +0600

    day 1 2 week 1

---
+++ day-1/script.js
@@ -0,0 +1,19 @@
+// JavaScript Keywords
+
+
+var Name = 25;
+var name = 20;
+const sabujHaowladerMiaSajib = '121'
+
+/*
+{
+    let sabujHaowladerMiaSajib = 'new value';
+       console.log(sabujHaowladerMiaSajib);
+       console.log(sabujHaowladerMiaSajib);
+       console.log(sabujHaowladerMiaSajib);
+       console.log(sabujHaowladerMiaSajib);
+}
+
+*/
+
+console.log(sabujHaowladerMiaSajib);
---
+++ day-2/operators.js
@@ -0,0 +1,18 @@
+// Arithmetic Operators
+//  what is arithmetic operator?
+//  Arithmetic operators are used to perform mathematical operations between numeric values (operands).
+
+let a = 2;
+let b = 2;      
+// console.log("Addition (a + b):", a + b);           // Addition
+// console.log("Subtraction (a - b):", a - b);        // Subtraction
+// console.log("Multiplication (a * b):", a * b);   // Multiplication  
+// console.log("Division (a / b):", a / b);          // Division
+// console.log("Modulus (a % b):", a % b);            // Modulus
+// console.log("Exponentiation (a ** b):", a ** b); // 2*2*2  Exponentiation
+
+console.log("Increment (a++):", a++);          // Increment
+console.log("Increment (a++):", a);          // Increment 1
+
+console.log("Decrement (b--):", b--);          // Decrement 
+console.log("Decrement (b--):", b);          // Decrement  1
