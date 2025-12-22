349c8c656f4d49fbeb248708b9fb2839d63d9881
Md Sabuj Mia <mdsobujredoy@gmail.com>
2025-12-22 22:05:49 +0600

    day 2 agian

--- day-1/script.js
+++ day-1/script.js
@@ -1,9 +1,8 @@
 // JavaScript Keywords
 
-
 var Name = 25;
 var name = 20;
-const sabujHaowladerMiaSajib = '121'
+const sabujHaowladerMiaSajib = "121";
 
 /*
 {
@@ -13,7 +12,17 @@ const sabujHaowladerMiaSajib = '121'
        console.log(sabujHaowladerMiaSajib);
        console.log(sabujHaowladerMiaSajib);
 }
-
 */
 
 console.log(sabujHaowladerMiaSajib);
+
+let a = 5;
+// let a = 10; // error: Identifier 'a' has already been declared
+a = 10; // valid
+//diffrence between let and const
+const b = 15;
+b = 20; // error: Assignment to constant variable.
+
+let c = 5;
+
+c = false;
--- day-2/operators.js
+++ day-2/operators.js
@@ -1,89 +1,99 @@
-// Arithmetic Operators
-//  what is arithmetic operator?
-//  Arithmetic operators are used to perform mathematical operations between numeric values (operands).
+// // Arithmetic Operators
+// //  what is arithmetic operator?
+// //  Arithmetic operators are used to perform mathematical operations between numeric values (operands).
 
-let a = 2;
-let b = 2;      
-// console.log("Addition (a + b):", a + b);           // Addition
-// console.log("Subtraction (a - b):", a - b);        // Subtraction
-// console.log("Multiplication (a * b):", a * b);   // Multiplication  
-// console.log("Division (a / b):", a / b);          // Division
-// console.log("Modulus (a % b):", a % b);            // Modulus
-// console.log("Exponentiation (a ** b):", a ** b); // 2*2*2  Exponentiation
+// let a = 2;
+// let b = 2;
+// // console.log("Addition (a + b):", a + b);           // Addition
+// // console.log("Subtraction (a - b):", a - b);        // Subtraction
+// // console.log("Multiplication (a * b):", a * b);   // Multiplication
+// // console.log("Division (a / b):", a / b);          // Division
+// // console.log("Modulus (a % b):", a % b);            // Modulus
+// // console.log("Exponentiation (a ** b):", a ** b); // 2*2*2  Exponentiation
 
-console.log("Increment (a++):", a++);          // Increment
-console.log("Increment (a++):", a);          // Increment 1
+// console.log("Increment (a++):", a++); // Increment
+// console.log("Increment (a++):", a); // Increment 1
 
-console.log("Decrement (b--):", b--);          // Decrement 
-console.log("Decrement (b--):", b);          // Decrement  1
+// console.log("Decrement (b--):", b--); // Decrement
+// console.log("Decrement (b--):", b); // Decrement  1
 
+// // Assignment Operators
 
-// Assignment Operators
+// //  what is assignment operator?
+// // Assignment operators are used to assign values to variables.
 
-//  what is assignment operator?
-// Assignment operators are used to assign values to variables.
+// let x = 10; // Simple assignment
+// x += 5; // x = x + 5
 
-let x = 10; // Simple assignment
-x += 5; // x = x + 5
+// console.log("Addition Assignment (x += 5):", x); // 15
 
-console.log("Addition Assignment (x += 5):", x); // 15  
+// let y = 20; // Simple assignment
+// y -= 5; // y = y - 5
 
-let y = 20; // Simple assignment
-y -= 5; // y = y - 5
+// console.log("Subtraction Assignment (y -= 5):", y); // 15
 
-console.log("Subtraction Assignment (y -= 5):", y); // 15
+// let m = 5; // Simple assignment
+// m *= 2; // m =m *2
+// console.log("Multiplication Assignment (m *= 2):", m); // 10
 
-let m = 5; // Simple assignment
-m *= 2; // m =m *2
-console.log("Multiplication Assignment (m *= 2):", m); // 10
+// let n = 20; // Simple assignment
+// n /= 4; // n = n / 4
+// console.log("Division Assignment (n /= 4):", n); // 5
 
-let n = 20; // Simple assignment
-n /= 4; // n = n / 4
-console.log("Division Assignment (n /= 4):", n); // 5
+// let p = 10; // Simple assignment
+// p %= 3; // p = p % 3
+// console.log("Modulus Assignment (p %= 3):", p); // 1
 
-let p = 10; // Simple assignment
-p %= 3; // p = p % 3                    
-console.log("Modulus Assignment (p %= 3):", p); // 1
+// // Logical Assignment Operators
+// // what is logical assignment operator?
+// // Logical assignment operators combine logical operations with assignment.
 
-// Logical Assignment Operators 
-// what is logical assignment operator?
-// Logical assignment operators combine logical operations with assignment.
+// const q = "";
+// const r = 25;
 
-const q = "";
-const r = 25;
+// console.log("check,", q || r);
 
-console.log('check,', q || r );
+// //Comparison Operators
+// //  what is comparison operator?
+// // Comparison operators are used to compare two values and return a boolean result (true or false).
 
+// let c = 10;
+// let d = 10;
 
-//Comparison Operators
-//  what is comparison operator?
-// Comparison operators are used to compare two values and return a boolean result (true or false).
+// console.log("Equal to (c == d):", c == d); // Equal to
 
+// console.log("Strict Equal to (c === d):", c === d); // Strict Equal to
 
-let c = 10;
-let d = 10;
+// console.log("Not Equal to (c != d):", c != d);
+// console.log("Not Equal to (c != d):", c != d);
+// console.log("Strict Not Equal to (c !== d):", c !== d); // Strict Not Equal to
 
-console.log("Equal to (c == d):", c == d); // Equal to
+// console.log("Greater than (d > c):", d > c); // Greater than
+// console.log("Greater than (d > c):", d < c); // Greater than
 
-console.log("Strict Equal to (c === d):", c === d); // Strict Equal to
+// console.log("Greater than or Equal to (c >= d):", c >= d); // Greater than or Equal to
+// console.log("Less than or Equal to (d <= c):", d <= c); // Less than or Equal to
 
-console.log("Not Equal to (c != d):", c != d);
-console.log("Not Equal to (c != d):", c != d);
-console.log("Strict Not Equal to (c !== d):", c !== d); // Strict Not Equal to
+// // string adition
+// console.log("Hello " + "World");
 
-console.log("Greater than (d > c):", d > c); // Greater than        
-console.log("Greater than (d > c):", d < c); // Greater than        
+// console.log(5 >= 5);
 
-console.log("Greater than or Equal to (c >= d):", c >= d); // Greater than or Equal to
-console.log("Less than or Equal to (d <= c):", d <= c); // Less than or Equal to
+// const e = 5;
+// e = e + 10;
+// e += 10;
 
+// const f = 20 * "30";
+// console.log(f); // 2030
+// console.log(typeof f); // string
 
+const f = 20 * "30";
+console.log(f); // 600
+console.log(typeof f); // number
 
+const obj = {
+  name: "sajib",
+};
+obj.age = 25;
 
-
-
-
-
-
-console.log(5 >= 5); 
-
+console.log(obj); // { name: 'sajib', age: 25 }
