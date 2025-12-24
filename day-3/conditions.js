fce8548f491c5d6bc21697fae10f4cb2a104539f
Md Sabuj Mia <mdsobujredoy@gmail.com>
2025-12-24 22:01:25 +0600

    day 4

--- day-3/conditions.js
+++ day-3/conditions.js
@@ -5,35 +5,32 @@
 // Use switch to specify many alternative code blocks to be executed
 // Use (? :) (ternary) as a shorthand for if...else
 
+const time = 6;
 
-const time = 2;
-
-if (!time) {
-    console.log("Good morning");
-}else{
-    console.log("Good evening");
+if (time === 6) {
+  console.log("Good morning");
+} else {
+  console.log("Good evening");
 }
 
-if (time > 10 ) {
-    console.log("Good day");
+if (time > 10) {
+  console.log("Good day");
 }
 
 // const age = 19
 let age = 55;
 
 if (age <= 54) {
-    console.log("You are a minor");
+  console.log("You are a minor");
 } else if (age !== 60) {
-    console.log("You just became an adult");
-}else {
-    console.log("You are an adult");
+  console.log("You just became an adult");
+} else {
+  console.log("You are an adult");
 }
 
-
 // switch statement
-
 switch (age) {
-  case  18:
+  case 18:
     console.log("You are a minor");
     break;
 
@@ -45,90 +42,104 @@ switch (age) {
     console.log("You just became an adult");
     break;
 
+  case 20:
+    console.log("You just became an adult");
+    break;
 
   default:
     console.log("You are an adult");
 }
 
-
 const day = 8;
 
 switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Invalid day");
-        break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid day");
+    break;
 }
-
 
 // Ternary Operator
 
+if (5 > 4) {
+  console.log("Good morning");
+} else {
+  console.log("Good evening");
+}
 
-console.log((5 !== 4) ? "Good morning" : "Good evening"); 
+console.log(5 > 4 ? "Good morning" : 5 == 5 ? "Good evening" : "Good night");
 
-
-
-//comparisons oparetor      
+//comparisons oparetor
 
 console.log(5 != 9);
 
-
-
 // 🔹 Equality and Inequality
-1 == 1 // true
-1 == '1' // true
-1 === '1' // false
-0 == false // true
-0 === false // false
+1 == 1; // true
+1 == "1"; // true
+1 === "1"; // false
+0 == false; // true
+0 === false; // false
 
+"hello" == "Hello"; //false
+null == undefined; ////true
 
-'hello' == 'Hello' //false
-null == undefined ////true
+null === undefined; ////false
 
-null === undefined ////false
+true == 1; // true
 
-true == 1 // true
+true === 1; // false
+false == 0; // true
+false === 0; // false
 
-true === 1 // false
-false == 0 // true
-false === 0 // false
-
-
-'5' != 5 // false
-'5' !== 5 // true
-
-
+"5" != 5; // false
+"5" !== 5; // true
 
 //NaN
 
 // 🔹 Greater Than / Less Than
-5 > 3 //true
-5 < 3 // false
-10 >= 10 //true
-7 <= 8 // true
-'apple' > 'banana' //false
-'abc' < 'bcd'
+5 > 3; //true
+5 < 3; // false
+10 >= 10; //true
+7 <= 8; // true
+"apple" > "banana"; //false
+"abc" < "bcd";
 
-'A' < 'a'
+"A" < "a";
 
-console.log('ac' < 'ab');
+console.log("ac" < "ab");
+
+// logical operators
+
+// 🔹 Logical AND (&&)
+
+if (true && false && true) {
+  console.log("Both conditions are true");
+}
+
+if (true || true) {
+  console.log("Both conditions are true");
+}
+const b = 10 > 5;
+if (!b) {
+  console.log("This condition is true");
+}
--- day-4/script.js
+++ day-4/script.js
@@ -1,4 +1,26 @@
+//loop in javascript
+// for loop print 1 to 10
+// in for loop we have 3 statements
+// 1. initialization
+// 2. condition
+// 3. increment/decrement
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
 // named function
+function greet(parameter) {
+  return document.write(`<h2>Hello World</h2>  
+  <p>This is my ${parameter} function</p>
+    <p>This is a code of block</p>
+  `);
+}
+
+greet("first");
+greet("second");
+greet("third");
+greet("fourth");
 
 function myFunctionForMultiplies(p1, p2) {
   return p1 * p2;
@@ -34,14 +56,12 @@ function myFunctionForAllCalculation(p1, p2, status) {
   }
 }
 
-console.log(myFunctionForAllCalculation(5, 6, "plus"));
-console.log(myFunctionForAllCalculation(6, 5, "minus"));
-console.log(myFunctionForAllCalculation(10, 2, "division"));
-console.log(myFunctionForAllCalculation(10, 2, "multiple"));
+// console.log(myFunctionForAllCalculation(5, 6, "plus"));
+// console.log(myFunctionForAllCalculation(6, 5, "minus"));
+// console.log(myFunctionForAllCalculation(10, 2, "division"));
+// console.log(myFunctionForAllCalculation(10, 2, "multiple"));
 
 // named function
-
-console.log(add(2, 3));
 
 function add(a, b) {
   return a + b;
@@ -52,10 +72,15 @@ const add2 = function (a, b) {
   return a + b;
 };
 
+const dummy = function (functionAsParameter) {
+  console.log("This is a dummy function");
+  return functionAsParameter(5, 10);
+};
+
+console.log(dummy(add2));
+
 const a = add2(2, 6);
 const b = add2(2, 5);
-
-console.log(b);
 
 // arrow function
 
@@ -65,7 +90,25 @@ const add3 = (a, b) => {
 
 const add4 = (a, b) => a + b;
 
-//6. Immediately Invoked Function Expression (IIFE)
+// 6. Immediately Invoked Function Expression (IIFE)
 (function () {
   console.log("IIFE executed");
 })();
+
+(function (name) {
+  console.log("Hello " + name);
+})("John Doe");
+
+// tamplate literals
+// it will help to create string more easily with dynamic data
+const personAge = 25;
+const personName = `sabuj ${personAge}`;
+
+const demoElement = document.getElementById("demo");
+demoElement.innerHTML = `Hello, my name is ${personName} and I am ${personAge} years old.`;
+demoElement.innerHTML += `<p>Next year, I will be ${
+  personAge + 1
+} years old.</p>`;
+demoElement.innerHTML += `<p>In five years, I will be ${
+  personAge + 5
+} years old.</p>`;
